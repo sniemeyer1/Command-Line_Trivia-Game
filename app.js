29c295b9605c28b508220b97a38a83da14354eb3
Savannah Niemeyer <savannahniemeyer@gmail.com>
2020-10-28 00:26:11 -0500

    now able to read questions and answers in console

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ let rawdata = fs.readFileSync('Apprentice_TandemFor400_Data.json');
 let questions = JSON.parse(rawdata);
 console.log(questions);
 
-for (i = 0; i < 5; i++){
-    console.log(`Q: ${questions[i].question}`)
+for (i = 0; i < questions.length; i++){
+    let question = questions[i].question;
+    let correctAnswer = questions[i].correct;
+    let choices = questions[i].incorrect;
+    choices.push(correctAnswer)
+    console.log(`Q:${question}
+    Choices: ${choices}
+
+    CA: ${correctAnswer}`)
 }
+//let answerArray = questions[i].
